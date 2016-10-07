f0b10d95ad80ad89ba1bf31212bca71fc7db024f
chengliu <chengliu@sohu-inc.com>
2016-10-07 19:48:52 +0800

    Init commit

---
+++ tests/test-basic.js
@@ -0,0 +1,42 @@
+var cconf = require('../index');
+var assert = require('assert');
+
+it("should merge ok", function() {
+    cconf.merge({
+        "a": "foo",
+        "b": "bar",
+        "c": {
+            "name": "Apple",
+            "shape": "round"
+        }
+    });
+    assert(cconf.get('a') == 'foo');
+
+    cconf.merge({
+        "a": "foo1",
+        "b": "bar1",
+        "c": {
+            "name": "Orange",
+            "color": "Yellow"
+        }
+    });
+    assert(cconf.get('a') == 'foo1');
+    assert(cconf.get('a') == 'foo1');
+    assert(cconf.get('c').shape === 'round');
+});
+
+it('should load json file ok', function() {
+    cconf.use('test2');
+    cconf.file('./tests/lib/global.json');
+    cconf.file('./tests/lib/env.json');
+    assert(cconf.get('a') == 'foo1');
+    assert(cconf.get('c').shape === 'round');
+});
+
+it('should load yaml file ok', function() {
+    cconf.use('test3');
+    cconf.file('./tests/lib/global.yaml');
+    cconf.file('./tests/lib/env.yaml');
+    assert(cconf.get('a') == 'foo1');
+    assert(cconf.get('c').shape === 'round');
+});
